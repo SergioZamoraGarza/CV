b32aa6dfb43643c415f8d60d8c3ce160b326561a
Serg10ZG94 <maximil.zheko@gmail.com>
2021-03-31 12:38:48 -0600

    Primer commit CV

---
+++ cv-proyecto/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {routing,appRoutingProviders} from './app.routing';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
